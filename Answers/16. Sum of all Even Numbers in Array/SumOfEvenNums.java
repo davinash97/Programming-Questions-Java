7feb4e8ca9c08f96f3c987f4c08b2135d9945b0d
D Avinash Kumar <avinash18.1234@gmail.com>
2023-09-05 13:01:49 +0530

    Change of Class Name, as it was throwing random warnings

--- Answers/16. Sum of all Even Numbers in Array/SumOfEvenNums.java
+++ Answers/16. Sum of all Even Numbers in Array/SumOfEvenNums.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SumOfEven {
+public class SumOfEvenNums {
     public static void main(String[] args) {
         System.out.print("Enter the size of Array\t: ");
         try (Scanner input = new Scanner(System.in)) {
